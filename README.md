da41117f31e4a7cb9f026c91152f23c3f59180d9
FindingDorri <tho.le.nas@gmail.com>
2022-03-01 17:40:43 -0700

    Added readme.md and  requirements.txt

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+# Required Module
+ 
+To install required modules for django, run this **INSIDE** of the virtual environment:
+```
+pip install -r requirements.txt
+```
--- ctrlIntell_project/wsgi.py
+++ ctrlIntell_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctrlIntell.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ctrlIntell_project.settings')
 
 application = get_wsgi_application()
